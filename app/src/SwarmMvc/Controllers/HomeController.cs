f43b99c1146840eafed3154a027c18cf6932a49c
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-09-30 00:24:06 +0200

    Feature: Added simple tools to view EC2 Instance ID in the web browser, so we can identify the different instances

--- app/src/SwarmMvc/Controllers/HomeController.cs
+++ app/src/SwarmMvc/Controllers/HomeController.cs
@@ -4,5 +4,32 @@ namespace SwarmMvc.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly HttpClient _httpClient;
+
+    public HomeController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var instanceInfo = await GetInstanceInfo();
+        ViewBag.InstanceInfo = instanceInfo;
+        return View();
+    }
+
+    private async Task<string> GetInstanceInfo()
+    {
+        try
+        {
+            // Try to get EC2 instance metadata
+            var response = await _httpClient.GetStringAsync("http://169.254.169.254/latest/meta-data/instance-id");
+            return $"Instance ID: {response}";
+        }
+        catch
+        {
+            // Fallback if not running on EC2 or metadata service unavailable
+            return $"Hostname: {Environment.MachineName}";
+        }
+    }
 }
